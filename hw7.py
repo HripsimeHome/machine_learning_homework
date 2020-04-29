dc54518b4cea783bdc845bd70221b5d47ad70a28
Hripsime <m_hripsime@mail.ru>
2020-04-29 23:22:58 +0500

    Added hw7

--- hw7.py
+++ hw7.py
@@ -21,7 +21,6 @@ df['sepal width (cm)'] = le.fit_transform(df['sepal width (cm)'])
 df['petal length (cm)'] = le.fit_transform(df['petal length (cm)'])
 df['petal width (cm)'] = le.fit_transform(df['petal width (cm)'])
 
-
 X = df[['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']]
 
 from sklearn.cluster import KMeans
@@ -30,5 +29,12 @@ model.fit(X)
 print(model.predict(X))
 
 import matplotlib.pyplot as plt
-plt.scatter(df['sepal length (cm)'], df['sepal width (cm)'], df['petal length (cm)'], df['petal width (cm)'])
+data = [[[1] * len(df['sepal length (cm)']), df['sepal length (cm)']],
+        [[2] * len(df['sepal width (cm)']) , df['sepal width (cm)']],
+        [[3] * len(df['petal length (cm)']) , df['petal length (cm)']],
+        [[4] * len(df['petal width (cm)']) , df['petal width (cm)']]]
+plt.scatter(data[0][0], data[0][1], color = 'blue')
+plt.scatter(data[1][0], data[1][1], color = "yellow")
+plt.scatter(data[2][0], data[2][1], color = "brown")
+plt.scatter(data[3][0], data[3][1], color = "green")
 plt.show()
